3fed3f26173e4e6f7fe65ff080d803e39a8a69c4
AlexandrosKaraman <alexandrosua1990@gmail.com>
2021-08-04 17:39:59 +0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,79 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Список задач </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-footer elevated>
+      <q-toolbar>
+        <q-toolbar-title>Footer</q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-grey-1">
+      <q-list>
+        <q-item-label header class="text-grey-8"> Новигация </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "Список задач",
+    icon: "list",
+    to: "/",
+  },
+  {
+    title: "Настройки",
+    icon: "settings",
+    to: "/settings",
+  },
+];
+
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
